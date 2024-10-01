42c22b68707c37f1d7ea441caf165a40d4326eb5
Arthur (@Refzlund) <arthur@refzlund.com>
2024-10-01 19:45:21 +0200

    Simplify a closure

--- src/routes/example/required.ts
+++ src/routes/example/required.ts
@@ -1,7 +1,9 @@
 import type { ValidationEvent } from '$lib'
 
 export function isRequired(e: ValidationEvent<number | string>, required?: boolean) {
-	if (required && (e.value === undefined || e.value === null || e.value === '')) {
+	if(!required) return
+
+	if (e.value === undefined || e.value === null || e.value === '') {
 		e.error('Required')
 	}
 }
